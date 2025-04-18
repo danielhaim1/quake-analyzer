9070d541a88844dc1b21a9614f6d34c61c688c89
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2025-04-18 13:19:29 +0200

    v0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="quake-analyzer",
-    version="0.1.1",
+    version="0.1.2",
     description="CLI tool to fetch and analyze earthquake data from USGS",
     long_description=long_description,
     long_description_content_type='text/markdown',
